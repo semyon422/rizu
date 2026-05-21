0533bb57dc20f39c9cc6acbfef2372e72fa5be5a
semyon422 <semyon422@users.noreply.github.com>
2026-05-21 16:48:53 +0500

    Fix bug in VisualEventsN2

--- chart/model/visual/VisualEventsN2.lua
+++ chart/model/visual/VisualEventsN2.lua
@@ -58,25 +58,39 @@ function VisualEventsN2:generate(vps, range)
 
 	for j = 1, #vps do
 		local vp = vps[j]
-		for i = 1, #vps do
-			local _vp = vps[i]  -- current time is from i to i+1
-			local rightTime = intersect(vps, j, i, range[2])
-			local leftTime = intersect(vps, j, i, range[1])
-			local speed = vp.localSpeed * _vp.currentSpeed
 
-			if rightTime then
-				table.insert(events, {
-					time = rightTime,
-					action = speed >= 0 and 1 or -1,
-					point = vp,
-				})
-			end
-			if leftTime then
-				table.insert(events, {
-					time = leftTime,
-					action = speed >= 0 and -1 or 1,
-					point = vp,
-				})
+		if vp.localSpeed == 0 then
+			table.insert(events, {
+				time = -math.huge,
+				action = -1,
+				point = vp,
+			})
+			table.insert(events, {
+				time = math.huge,
+				action = -1,
+				point = vp,
+			})
+		else
+			for i = 1, #vps do
+				local _vp = vps[i]  -- current time is from i to i+1
+				local rightTime = intersect(vps, j, i, range[2])
+				local leftTime = intersect(vps, j, i, range[1])
+				local speed = vp.localSpeed * _vp.currentSpeed
+
+				if rightTime then
+					table.insert(events, {
+						time = rightTime,
+						action = speed >= 0 and 1 or -1,
+						point = vp,
+					})
+				end
+				if leftTime then
+					table.insert(events, {
+						time = leftTime,
+						action = speed >= 0 and -1 or 1,
+						point = vp,
+					})
+				end
 			end
 		end
 	end
