44a4a3785292732de18c0dc0f7cd84219a19123a
Nimue <83221369+Nimue-lua@users.noreply.github.com>
2026-06-28 21:17:27 +0300

    Remove unused strategies

--- gui/composition/Strategies.lua
+++ gui/composition/Strategies.lua
@@ -1,12 +1,9 @@
 ---@class gui.composition.Strategies
 local Strategies = {
 	Stack = require("gui.composition.Stack"),
-	Column = require("gui.composition.Column"),
 	Flow = require("gui.composition.Flow"),
 	Track = require("gui.composition.Track"),
 	Anchor = require("gui.composition.Anchor"),
-	Align = require("gui.composition.Align"),
-	Fixed = require("gui.composition.Fixed")
 }
 
 return Strategies
