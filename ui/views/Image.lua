38f1ea4f6b40b9a8160d7182d38bc091826cafb6
Nimue <83221369+Nimue-lua@users.noreply.github.com>
2026-07-26 17:41:27 +0300

    Add color field to Rectangle

--- ui/views/Image.lua
+++ ui/views/Image.lua
@@ -1,4 +1,5 @@
 local View = require("gui.View")
+local Painter = require("gui.Painter")
 
 ---@alias ui.views.ImageMode "native"|"fit"
 
@@ -14,11 +15,13 @@ local Image = View + {}
 ---@param texture love.Texture
 ---@param quad love.Quad
 ---@param mode ui.views.ImageMode?
-function Image:new(texture, quad, mode)
+---@param color gui.Color?
+function Image:new(texture, quad, mode, color)
 	View.new(self)
 	self.texture = texture
 	self.quad = quad
 	self.mode = mode or "native"
+	self.color = color or {1, 1, 1, 1}
 	assert(self.mode == "native" or self.mode == "fit", "invalid image mode")
 
 	local _, _, width, height = self.quad:getViewport()
@@ -32,6 +35,7 @@ end
 local lg = love.graphics
 
 function Image:draw()
+	Painter.setColorTable(self.color)
 	if self.mode == "fit" then
 		lg.draw(self.texture, self.quad, 0, 0, 0, self.width / self.image_width, self.height / self.image_height)
 		return
