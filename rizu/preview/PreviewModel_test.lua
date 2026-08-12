20a4a3803e440598a61a9af0d1f84a2d598d1f58
semyon422 <semyon422@users.noreply.github.com>
2026-08-12 21:45:46 +0500

    Type preview model fixtures

--- rizu/preview/PreviewModel_test.lua
+++ rizu/preview/PreviewModel_test.lua
@@ -2,6 +2,20 @@ local PreviewModel = require("rizu.preview.PreviewModel")
 
 local test = {}
 
+---@class rizu.preview.FakeAudioPreviewPlayer
+---@field pauseCount integer
+---@field stopCount integer
+---@field pause fun(self: rizu.preview.FakeAudioPreviewPlayer)
+---@field stop fun(self: rizu.preview.FakeAudioPreviewPlayer)
+
+---@class rizu.preview.FakeBgaPreviewPlayer
+---@field stopCount integer
+---@field stop fun(self: rizu.preview.FakeBgaPreviewPlayer)
+
+---@class rizu.preview.FakeChartPreview
+---@field chartview string|rizu.preview.PreviewChartview?
+---@field setChartview fun(self: rizu.preview.FakeChartPreview, chartview: rizu.preview.PreviewChartview?)
+
 local function createConfigModel()
 	return {
 		configs = {
@@ -33,6 +47,7 @@ end
 ---@return rizu.preview.PreviewModel
 local function createPreviewModel()
 	local previewModel = PreviewModel(createConfigModel(), {}, {})
+	---@type rizu.preview.FakeAudioPreviewPlayer
 	previewModel.audioPreviewPlayer = {
 		pauseCount = 0,
 		stopCount = 0,
@@ -43,12 +58,14 @@ local function createPreviewModel()
 			self.stopCount = self.stopCount + 1
 		end,
 	}
+	---@type rizu.preview.FakeBgaPreviewPlayer
 	previewModel.bgaPreviewPlayer = {
 		stopCount = 0,
 		stop = function(self)
 			self.stopCount = self.stopCount + 1
 		end,
 	}
+	---@type rizu.preview.FakeChartPreview
 	previewModel.chartPreview = {
 		chartview = "existing",
 		setChartview = function(self, chartview)
@@ -62,9 +79,17 @@ end
 function test.stop_disables_preview_until_loaded_again(t)
 	local previewModel = createPreviewModel()
 
+	local chartview = {
+		hash = "hash",
+		location_path = "song.sph",
+		location_prefix = "",
+		location_dir = "",
+		chartfile_name = "song.sph",
+		index = 1,
+	}
 	previewModel:load()
 	previewModel:stop()
-	previewModel:setAudioPathPreview("song.ogg", 12, "absolute", {hash = "hash"})
+	previewModel:setAudioPathPreview("song.ogg", 12, "absolute", chartview)
 	previewModel:update()
 
 	t:eq(previewModel.active, false)
@@ -76,7 +101,7 @@ function test.stop_disables_preview_until_loaded_again(t)
 	t:eq(previewModel.chartPreview.chartview, nil)
 
 	previewModel:load()
-	previewModel:setAudioPathPreview("song.ogg", 12, "absolute", {hash = "hash"})
+	previewModel:setAudioPathPreview("song.ogg", 12, "absolute", chartview)
 
 	t:eq(previewModel.active, true)
 	t:eq(previewModel.audio_path, "song.ogg")
