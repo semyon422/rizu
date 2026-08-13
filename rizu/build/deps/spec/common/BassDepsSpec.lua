4d68f44f93992300e918e10c3cd05f4007adc792
semyon422 <semyon422@users.noreply.github.com>
2026-08-13 12:42:56 +0500

    Type Bass dependency spec

--- rizu/build/deps/spec/common/BassDepsSpec.lua
+++ rizu/build/deps/spec/common/BassDepsSpec.lua
@@ -3,13 +3,21 @@ local Manifest = require("rizu.build.deps.Manifest")
 ---@class rizu.build.deps.spec.common.BassDepsSpec
 local BassDepsSpec = {}
 
+---@alias rizu.build.deps.BassDependency "bass"|"bassmix"|"bass_fx"
+
+---@type rizu.build.deps.BassDependency[]
 local BASS_DEPS = {"bass", "bassmix", "bass_fx"}
+---@type {[rizu.build.deps.BassDependency]: {[rizu.build.Target]: string}}
 local OUTPUT_NAME = {
 	bass = {linux = "libbass.so", windows = "bass.dll", macos = "libbass.dylib"},
 	bassmix = {linux = "libbassmix.so", windows = "bassmix.dll", macos = "libbassmix.dylib"},
 	bass_fx = {linux = "libbass_fx.so", windows = "bass_fx.dll", macos = "libbass_fx.dylib"},
 }
 
+---@param dep_name rizu.build.deps.BassDependency
+---@param target rizu.build.Target
+---@param extract string
+---@return string?
 local function getSourcePath(dep_name, target, extract)
 	local name = OUTPUT_NAME[dep_name] and OUTPUT_NAME[dep_name][target]
 	if not name then
@@ -28,12 +36,14 @@ end
 ---@param spec rizu.build.deps.Spec
 function BassDepsSpec.add(target, spec)
 	for _, dep_name in ipairs(BASS_DEPS) do
-		local cfg = Manifest[dep_name] and Manifest[dep_name][target]
+		local entries = Manifest[dep_name] --[[@as {[rizu.build.Target]: rizu.build.deps.ManifestEntry}]]
+		local cfg = entries[target]
 		if cfg then
 			local archive = "${downloads_dir}/" .. cfg.archive
-			local extract = "${deps_dir}/" .. dep_name .. "_" .. target
-			local src = getSourcePath(dep_name, target, extract)
-			local out_name = OUTPUT_NAME[dep_name] and OUTPUT_NAME[dep_name][target]
+			local extract = ("${deps_dir}/%s_%s"):format(dep_name, target)
+			local src = assert(getSourcePath(dep_name, target, extract))
+			local out_name = OUTPUT_NAME[dep_name][target]
+			---@type rizu.build.deps.Action[]
 			local actions = {
 				{type = "download", url = cfg.url, dest = archive},
 				{type = "extract", format = "zip", archive = archive, dest = extract},
@@ -43,7 +53,7 @@ function BassDepsSpec.add(target, spec)
 			table.insert(actions, {type = "copy_exact", src = src, dst = "${bin_dir}/" .. out_name, flags = "-f"})
 
 			table.insert(spec.steps, {
-				id = "dep_" .. dep_name,
+				id = ("dep_%s"):format(dep_name),
 				kind = "archive",
 				outputs = {"${bin_dir}/" .. out_name},
 				actions = actions,
