41a0167659bdcb4c55c4579eca0751eeebc8988d
Nimue <83221369+Nimue-lua@users.noreply.github.com>
2026-08-16 10:07:40 +0300

    Add new packages

--- sphere/pkg/repo.lua
+++ sphere/pkg/repo.lua
@@ -12,6 +12,18 @@ local repo = {
 		url = "https://codeload.github.com/Thetan-ILW/ManipFactorEtterna-soundsphere/zip/refs/heads/main",
 		source = "https://github.com/Thetan-ILW/ManipFactorEtterna-soundsphere",
 	},
+	{
+		name = "daitergg/more_modifiers",
+		display_name = "More Modifiers",
+		url = "https://codeload.github.com/DaiterGG/more_modifiers/zip/refs/heads/main",
+		source = "https://github.com/DaiterGG/more_modifiers"
+	},
+	{
+		name = "daitergg/input_preview",
+		display_name = "Input Preview skin element",
+		url = "https://codeload.github.com/DaiterGG/input_preview/zip/refs/heads/main",
+		source = "https://github.com/DaiterGG/input_preview"
+	}
 }
 
 return repo
